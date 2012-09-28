5c5c1b8e212844032e108d60e22c31d2e8b9b97c
jerry8huang <jerry8huang@gmail.com>
2012-09-29 01:19:26 +0800

    Update HelloJerry/HelloJerry/src/hello/HelloWorld.java

--- HelloJerry/HelloJerry/src/hello/HelloWorld.java
+++ HelloJerry/HelloJerry/src/hello/HelloWorld.java
@@ -3,7 +3,7 @@ package hello;
 public class HelloWorld {
    public static void main(String[] args){
 	   
-	   System.out.println("Hell0 World!");
+	   System.out.println("Hell0 World!Fuck man");
    }
 	   
 	
