ea22b758965ad29be12de733f2dbeaccaf9940e1
jerry8huang <jerry8huang@gmail.com>
2012-09-28 12:32:53 +0800

    Update HelloJerry/HelloJerry/src/hello/HelloWorld.java

--- HelloJerry/HelloJerry/src/hello/HelloWorld.java
+++ HelloJerry/HelloJerry/src/hello/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
    public static void main(String[] args){
 	   
 	   System.out.println("Hell0 World!!");
-	   System.out.println("Hell0 Money000");
+	   System.out.println("Hell0 Money0);
    }
 	
 }
