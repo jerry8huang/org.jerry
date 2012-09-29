6f1327f87613c4bae11327111c2d39b0d5fa8369
jerry8huang <jerry8huang@gmail.com>
2012-09-29 22:15:31 +0800

    add syste.out in Lower.

--- HiveLower/src/udf/Lower.java
+++ HiveLower/src/udf/Lower.java
@@ -5,8 +5,14 @@ import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.io.Text;
 
 public final class Lower extends UDF {
+	
   public Text evaluate(final Text s) {
+	  System.out.println("456");
     if (s == null) { return null; }
     return new Text(s.toString().toLowerCase());
+    
+    
   }
+  
+  
 }
