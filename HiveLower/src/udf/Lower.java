0bfe1f71fc99052970aa65ce15cf063a2278883a
jerry8huang <jerry8huang@gmail.com>
2012-10-09 17:48:59 +0800

    updated HiveLower from redhat6

--- HiveLower/src/udf/Lower.java
+++ HiveLower/src/udf/Lower.java
@@ -7,7 +7,7 @@ import org.apache.hadoop.io.Text;
 public final class Lower extends UDF {
 	
   public Text evaluate(final Text s) {
-	  System.out.println("456");
+	  System.out.println("456789");
     if (s == null) { return null; }
     return new Text(s.toString().toLowerCase());
     
