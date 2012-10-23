ca800cfa094eb7b5c93d48c3d96e37ce93565569
jerry8huang <jerry8huang@gmail.com>
2012-10-24 01:44:26 +0800

    input with a File to test WordMap

--- HelloJerry/HelloJerry/src/test/WordMap.java
+++ HelloJerry/HelloJerry/src/test/WordMap.java
@@ -1,26 +1,33 @@
 package test;
 
 import java.util.HashMap;
-
-
+import java.io.*;
+import java.util.*;
 
 public class WordMap {
 
  
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+    	char[] charBuf=new char[10];
+    	
+    	ArrayList<String> input=new ArrayList<String>();
+    	FileReader FrIn=new FileReader(args[0]);
+    	while(FrIn.read(charBuf)>0){
+    		
+    	input.add(new String(charBuf));
+    		}
+       //String input = "a1,b2,3c,d4d,a1,b2,a1";
+    	String inFile=input.toString();
+        String[] ws = inFile.split(",");
+    	 
 
-       String input = "a1,b2,3c,d4d,a1,b2,a1";
-
-       String[] ws = input.split(",");
-
-//做的单词分割，
-
-//如果你要空格分割 String[] ws = input.split(" ");
+//鍋氱殑鍗曡瘝鍒嗗壊锛�
+//濡傛灉浣犺绌烘牸鍒嗗壊 String[] ws = input.split(" ");
 
        HashMap<String,Integer> hm = new HashMap<String, Integer>();
 
-//初始化HashMap
+//鍒濆鍖朒ashMap
 
        for(String s:ws)
 
@@ -28,18 +35,17 @@ public class WordMap {
 
            if(s.matches("[a-zA-Z]\\w+"))
 
-//这里是一个正则表达式，判断的是第一个必须是字母，后面的是一个字母或多个字母，一个//数字或多个数字
-
+//杩欓噷鏄竴涓鍒欒〃杈惧紡锛屽垽鏂殑鏄涓�釜蹇呴』鏄瓧姣嶏紝鍚庨潰鐨勬槸涓�釜瀛楁瘝鎴栧涓瓧姣嶏紝涓�釜//鏁板瓧鎴栧涓暟瀛�
            {
         	  System.out.println("s is:"+s);
               Integer cou = hm.get(s);
               System.out.println("cou is:"+cou);
 
-//get()方法将产生一个与键相关联的Integer值，然后这个值被递增，为的是记录标识符的//个数
+//get()鏂规硶灏嗕骇鐢熶竴涓笌閿浉鍏宠仈鐨処nteger鍊硷紝鐒跺悗杩欎釜鍊艰閫掑锛屼负鐨勬槸璁板綍鏍囪瘑绗︾殑//涓暟
 
               hm.put(s, cou == null?1:cou+1);
 
-//保存到HashMap中以单词做key,判断单词是否为空，空为1，或则加1
+//淇濆瓨鍒癏ashMap涓互鍗曡瘝鍋歬ey,鍒ゆ柇鍗曡瘝鏄惁涓虹┖锛岀┖涓�锛屾垨鍒欏姞1
 
            }
 
@@ -47,7 +53,7 @@ public class WordMap {
       // hm.remove("b2");
        System.out.println(hm);
 
-//打印HashMap
+//鎵撳嵃HashMap
 
     }
 
