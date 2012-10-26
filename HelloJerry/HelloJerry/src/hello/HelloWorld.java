407ce438740b9f7ce3fba92ee91ae7f876eefee3
jerry8redhat <jerry8huang@gmail.com>
2012-10-26 18:02:25 +0800

    updated HelloWorld to test hashCode() of String

--- HelloJerry/HelloJerry/src/hello/HelloWorld.java
+++ HelloJerry/HelloJerry/src/hello/HelloWorld.java
@@ -2,8 +2,12 @@ package hello;
 
 public class HelloWorld {
    public static void main(String[] args){
-	   
-	   System.out.println("Hell0 money!Fuck man");
+	   int i=0;
+	   char c='r';
+	   i=i*5+c;
+	   String r="rr";
+	   System.out.println("Hell0 money!Fuck man;"+i+c);
+	   System.out.println(r.hashCode());
    }
 	   
 	
